324692d56f03adcfc97090c0d152e77902a08a24
Vasilii Stepanov <vstepanov@devexperts.com>
2018-08-13 18:39:32 +0300

    ktor

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,6 +1,15 @@
-import khttp.get
-import org.json.JSONArray
-import org.json.JSONObject
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.request.accept
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.util.AttributeKey
+import io.ktor.util.Attributes
+import kotlinx.coroutines.experimental.runBlocking
 import java.net.URLEncoder
 import java.time.Instant
 import java.time.LocalDateTime
@@ -12,9 +21,12 @@ fun main(args: Array<String>) {
     // there is a project define the most committees for a week
     val start = System.currentTimeMillis() - TimeUnit.DAYS.toMillis(2)
     val end = System.currentTimeMillis() - TimeUnit.DAYS.toMillis(1)
-    val requestParameters = mapOf(Pair("Authorization", "Basic c3RlNzAzOm1vaHdvbzZHUEBzcw=="), Pair("Accept", "application/json"))
-    //println(readGitCommittedLines("tos", start, end, requestParameters))
-    println(readJiraContentModifications("tos", start, end, requestParameters))
+
+    val password = args[0]
+    val requestAttributes = Attributes()
+    requestAttributes.put(AttributeKey("Authorization"), "Basic $password")
+    //println(readGitCommittedLines("tos", start, end, requestAttributes))
+    println(readJiraContentModifications("tos", start, end, requestAttributes))
 }
 
 const val FISH_EYE_URL = "https://tosfish.iteclientsys.local/rest-service-fe"
@@ -29,13 +41,13 @@ val DATA_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd")!!
 // https://tosfish.iteclientsys.local/rest-service-fe/revisionData-v1/changesetList/tos
 // https://tosfish.iteclientsys.local/rest-service-fe/revisionData-v1/changeset/tos/a9e174976d0586555c1d10c375e48490671d025c
 // https://tosfish.iteclientsys.local/rest-service-fe/revisionData-v1/revisionInfo/tos/?path=AdminGUI/src/main/java/com/devexperts/tos/ui/admin/riskmonitor/RiskPanel.java&revision=7f8cdb314790e65e82bc6ca1af846b7e259e88bf
-fun readGitCommittedLines(project: String, fromTime: Long, toTime: Long, requestParameters: Map<String, String>): Map<String, Int> {
+fun readGitCommittedLines(project: String, fromTime: Long, toTime: Long, requestAttributes: Attributes): Map<String, Int> {
     val from = formatTime(fromTime)
     val to = formatTime(toTime)
     var userLines: Map<String, Int> = mutableMapOf()
     val query = URLEncoder.encode("select revisions where date in [$from, $to) return author, linesAdded, linesRemoved", "utf-8")
-    val statistics = get("$FISH_EYE_URL/search-v1/queryAsRows/$project?query=$query", requestParameters).jsonObject
-    for (row in statistics["row"] as JSONArray) {
+    val statistics = get("$FISH_EYE_URL/search-v1/queryAsRows/$project?query=$query", requestAttributes)
+    for (row in statistics["row"] as JsonArray) {
         val item = jA(jO(row)["item"])
         val lines = userLines.getOrDefault(item[0] as String, 0)
         userLines = userLines.plus(Pair(item[0] as String, lines + item[1] as Int + item[2] as Int))
@@ -46,12 +58,12 @@ fun readGitCommittedLines(project: String, fromTime: Long, toTime: Long, request
 // confluence
 //  https://docs.atlassian.com/ConfluenceServer/rest/6.10.1/#content-getContentById
 fun readConfluenceContentModifications(space: String, fromTime: Long, toTime: Long,
-                                       requestParameters: Map<String, String>): Map<String, Int> {
+                                       requestAttributes: Attributes): Map<String, Int> {
     val from = formatTime(fromTime)
     val to = formatTime(toTime)
     val query = URLEncoder.encode("space=$space and (lastmodified >= $from and lastmodified <= $to " +
             "or created >= $from and created <= $to", "utf-8")
-    val statistics = get("$CONFLUENCE_URL/search?cql=$query&expand=version", requestParameters).jsonObject
+    val statistics = get("$CONFLUENCE_URL/search?cql=$query&expand=version", requestAttributes)
     var writtenLines: Map<String, Int> = mutableMapOf()
     for (result in jA(statistics["results"])) {
         val updater = jO(jO(jO(result)["version"])["by"])["username"] as String
@@ -66,12 +78,12 @@ fun readConfluenceContentModifications(space: String, fromTime: Long, toTime: Lo
 // https://tosjira.iteclientsys.local/rest/api/2/search
 // project = alerts project = alerts and (updated >= 2018-08-01 and  updated <= 2018-08-03 or created >= 2018-08-01 and  created <= 2018-08-03)
 fun readJiraContentModifications(project: String, fromTime: Long, toTime: Long,
-                                 requestParameters: Map<String, String>): Map<String, Int> {
+                                 requestAttributes: Attributes): Map<String, Int> {
     val from = formatTime(fromTime)
     val to = formatTime(toTime)
     val query = URLEncoder.encode("project=$project and (updated >= $from and updated <= $to or " +
             "created >= $from and created <= $to)", "utf-8")
-    val statistics = get("$JIRA_URL/search?jpl=$query&fields=changelog&expand=changelog", requestParameters).jsonObject
+    val statistics = get("$JIRA_URL/search?jpl=$query&fields=changelog&expand=changelog", requestAttributes)
     var modifiedTimes: Map<String, Int> = mutableMapOf()
     for(issue in jA(statistics["issues"])) {
         for(change in jA(jO(jO(issue)["changelog"])["histories"])) {
@@ -85,6 +97,26 @@ fun readJiraContentModifications(project: String, fromTime: Long, toTime: Long,
     return modifiedTimes
 }
 
-fun jO(any: Any): JSONObject = any as JSONObject
-fun jA(any: Any): JSONArray = any as JSONArray
-fun formatTime(time: Long) = DATA_FORMATTER.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(time), ZoneOffset.UTC))]
+
+fun get(url: String, requestAttributes: Attributes): JsonObject {
+    val client = HttpClient(Apache) {
+        install(JsonFeature) {
+            serializer = GsonSerializer {
+                // .GsonBuilder
+                serializeNulls()
+                disableHtmlEscaping()
+            }
+        }
+    }
+    return runBlocking {
+        client.get<JsonObject>(url) {
+            accept(ContentType.Application.Json)
+            setAttributes {
+                requestAttributes
+            }
+        }
+    }
+}
+fun jO(any: Any): JsonObject = any as JsonObject
+fun jA(any: Any): JsonArray = any as JsonArray
+fun formatTime(time: Long) = DATA_FORMATTER.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(time), ZoneOffset.UTC))
